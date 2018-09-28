48fcde5035c0b2530ffbc72274c0b2fb531c1bfa
Anh Nguyen <zachnguyen03@Anhs-MacBook-Pro.local>
2018-09-28 11:38:14 +0300

    Added input fields read only and editable when submitting and resetting

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 const form = document.getElementById("myForm");
 const ProductCatalogue = document.getElementById("catalogue");
-let UserFirstName = document.getElementById("user-firstname").value;
-let UserLastName = document.getElementById("user-lastname").value;
-let UserAddress = document.getElementById("user-address").value;
+let UserFirstName = document.getElementById("user-firstname");
+let UserLastName = document.getElementById("user-lastname");
+let UserAddress = document.getElementById("user-address");
 const products = [
     {
         name: 'RTX 2080',
@@ -20,13 +20,26 @@ const products = [
 
 function loadLocalStorage() {
     localStorage.setItem("catalogue", JSON.stringify(products));
-};
+}
 
 function resetFields() {
-    UserFirstName = '';
-    UserLastName = '';
-    UserAddress = '';
+    UserFirstName.value = '';
+    UserLastName.value = '';
+    UserAddress.value = '';
     ProductCatalogue.innerHTML = "";
+    inputsEditable();
+}
+
+function inputsReadOnly() {
+    UserFirstName.readOnly = true;
+    UserLastName.readOnly = true;
+    UserAddress.readOnly = true;
+}
+
+function inputsEditable() {
+    UserFirstName.readOnly = false;
+    UserLastName.readOnly = false;
+    UserAddress.readOnly = false;
 }
 
 
@@ -34,7 +47,8 @@ form.addEventListener("submit", e => {
     e.preventDefault();
     var Products = JSON.parse(localStorage.getItem("catalogue"))
     ProductCatalogue.innerHTML = "";
-    if(UserFirstName !== null && UserLastName !== null && UserAddress !== null) {
+    if(UserFirstName.value !== null && UserLastName.value !== null && UserAddress.value !== null) {
+        inputsReadOnly();
         for(var i=0; i < Products.length; i++) {
             ProductName = Products[i].name;
             ProductPrice = Products[i].price;
